7a5f989ef060745576235a9cbd0b0fc95c461349
Astro-Syn <kelseyamyb@gmail.com>
2026-05-15 08:48:56 -0400

    add more setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
 
 const scene = new THREE.Scene();
 
@@ -12,4 +14,35 @@ const cubeMesh = new THREE.Mesh(
 
 scene.add(cubeMesh)
 
-console.log(scene)
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,
+  0.5,
+  200)
+
+camera.position.z = 5
+
+
+
+const canvas = document.querySelector('canvas.threejs')
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
+
+
+const controls = new OrbitControls( camera, canvas)
+
+const renderLoop = () => {
+    window.requestAnimationFrame(renderLoop);
+    
+}
+
+console.log(renderLoop)
+
+
+controls.update()
+
+scene.add(camera)
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.render(scene, camera)
